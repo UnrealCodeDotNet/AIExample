d7fadb5dfc40597d42b83d77f66cd65e2650ebaa
JohnJFarrow <105021048+JohnJFarrow@users.noreply.github.com>
2024-07-14 15:56:52 +1200

    initial commit

---
+++ Source/AIExample/Public/SetProperties.h
@@ -0,0 +1,33 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+
+// utilities for setting properties
+
+inline bool SetBlackboardKeySelectorProperty( UObject * Target, const FName& VarName, const FBlackboardKeySelector& NewValue )
+{
+	if (Target)
+	{
+		if (const FStructProperty* Prop = FindFieldChecked<FStructProperty>(Target->GetClass(), VarName ))
+		{
+			Prop->SetValue_InContainer( Target, &NewValue ); 
+			return true;
+		}
+	}
+	return false;
+}	
+
+inline bool SetBoolProperty( UObject * Target, const FName& VarName, const bool NewValue )
+{
+	if (Target)
+	{
+		if (const FBoolProperty* Prop = FindFieldChecked<FBoolProperty>(Target->GetClass(), VarName ))
+		{
+			Prop->SetPropertyValue_InContainer( Target, NewValue ); 
+			return true;
+		}
+	}
+	return false;
+}	
